64c1fe402e4a3b3c7044c2918e85c69278250468
Hirwa Hope <gakundohope5@gmail.com>
2024-09-25 07:15:22 +0200

    port number

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
-const PORT = 3000;
+const PORT =process.env.PORT||3000;
 
 // Middleware
 app.use(cors());
